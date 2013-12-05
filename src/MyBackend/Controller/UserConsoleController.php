2075bde0815cd4c765d77819142f3f819a5a826c
Stefano Torresi <webdev@stefanotorresi.it>
2013-12-05 13:30:09 +0100

    add cli user controller class

---
+++ src/MyBackend/Controller/UserConsoleController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Stefano Torresi (http://stefanotorresi.it)
+ * @license See the file LICENSE.txt for copying permission.
+ * ************************************************
+ */
+
+namespace MyBackend\Controller;
+
+use MyBase\Controller\AbstractConsoleController;
+use MyBackend\Entity;
+
+class UserConsoleController extends AbstractConsoleController
+{
+    public function createAdminAction()
+    {
+        
+    }
+}
